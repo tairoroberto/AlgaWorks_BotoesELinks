8fd2bdb342ed59d637a4cb0b2081f690c96cc3b6
Tairo Roberto <tairoroberto@hotmail.com>
2015-02-16 23:40:34 -0200

    Botões e links curso JavaJSF - Tairo

---
+++ src/br/com/tairoroberto/botoeslinks/FormularioBean.java
@@ -0,0 +1,18 @@
+package br.com.tairoroberto.botoeslinks;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class FormularioBean {
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
